05ee8cea03399f0dcc3354636d8623d9b3ea9703
ben carpenter <bencarpenter79@gmail.com>
2018-01-16 15:38:56 -0800

    BenC added comments to app-start

--- TestApplication/TestApplication/Global.asax.cs
+++ TestApplication/TestApplication/Global.asax.cs
@@ -12,7 +12,7 @@ namespace TestApplication
         protected void Application_Start(object sender, EventArgs e)
         {
             //this is my change to test-git by Ben Carpenter
-            //tjis is a different change  by Ben Carpenter
+            //this is a different change  by Ben Carpenter
         }
     }
 }
